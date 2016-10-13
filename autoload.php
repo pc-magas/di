2604707e41cab1e7fd929a7bb920fe8150b9dbdd
Dimitrios Desillas <ddesillas@yahoo.gr>
2016-10-13 14:29:18 +0300

    Fix autoload

--- autoload.php
+++ autoload.php
@@ -1,16 +1,16 @@
 <?php
 
-function require_all_files($root) 
+function require_all_files($root)
 {
     $d = new RecursiveDirectoryIterator($root);
-    foreach (new RecursiveIteratorIterator($d) as $file => $f) 
+    foreach (new RecursiveIteratorIterator($d) as $file => $f)
     {
         $ext = pathinfo($f, PATHINFO_EXTENSION);
         if ($ext == 'php' || $ext == 'inc')
 		{
-			require_once ($file); // or require(), require_once(), include_once()		
-		}		
+			require_once ($file); // or require(), require_once(), include_once()
+		}
     }
 }
 
-require_all_files('./src');
+require_all_files(__DIR__.'src');
