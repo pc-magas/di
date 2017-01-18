fa92b9095f11bf416b5fc9f40c32518175f22cf8
Dimitrios Desyllas <ddesyllas@25-8projects.com>
2017-01-18 12:29:11 +0200

    Fix autoload

--- autoload.php
+++ autoload.php
@@ -1,16 +1,23 @@
 <?php
 
-function require_all_files($root)
-{
-    $d = new RecursiveDirectoryIterator($root);
-    foreach (new RecursiveIteratorIterator($d) as $file => $f)
+//Namespace in order to avoid conflicts during autoload
+namespace DiLoader{
+
+    function require_all_files($root)
     {
-        $ext = pathinfo($f, PATHINFO_EXTENSION);
-        if ($ext == 'php' || $ext == 'inc')
-    		{
-    			require_once ($file); // or require(), require_once(), include_once()
-    		}
+        $d = new RecursiveDirectoryIterator($root);
+        foreach (new RecursiveIteratorIterator($d) as $file => $f)
+        {
+            $ext = pathinfo($f, PATHINFO_EXTENSION);
+            if ($ext == 'php' || $ext == 'inc')
+                {
+                    require_once ($file); // or require(), require_once(), include_once()
+                }
+        }
     }
 }
 
-require_all_files(__DIR__.'/src');
+namespace {
+    DiLoader\require_all_files(__DIR__.'/src');
+}
+
